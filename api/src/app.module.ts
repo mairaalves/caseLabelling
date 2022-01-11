a23308f1b790408c5158fa10a2f65bb3bcc86a65
mairaalves <marry.alves@hotmail.com>
2022-01-11 14:29:49 -0300

    [add] auth module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConditionModule } from './modules/condition/condition.module';
 import { CaseModule } from './modules/case/case.module';
+import { ConditionModule } from './modules/condition/condition.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 const dbUrl = `mongodb://${process.env.DATA_BASE}:${process.env.MONGO_DB_PORT}/`;
 
 @Module({
-  imports: [CaseModule, ConditionModule, MongooseModule.forRoot(dbUrl)],
+  imports: [CaseModule, ConditionModule, AuthModule, MongooseModule.forRoot(dbUrl)],
 })
 export class AppModule {}
