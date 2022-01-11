3dce168eca4b26766e83c703ab160313b039e422
mairaalves <marry.alves@hotmail.com>
2022-01-11 14:31:31 -0300

    [add] auth middleware

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { CaseModule } from './modules/case/case.module';
 import { ConditionModule } from './modules/condition/condition.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { AuthMiddleware } from './middlewares/auth.middleware';
+import { CaseController } from './modules/case/case.controller';
+import { ConditionController } from './modules/condition/condition.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 
 const dbUrl = `mongodb://${process.env.DATA_BASE}:${process.env.MONGO_DB_PORT}/`;
@@ -9,4 +12,10 @@ const dbUrl = `mongodb://${process.env.DATA_BASE}:${process.env.MONGO_DB_PORT}/`
 @Module({
   imports: [CaseModule, ConditionModule, AuthModule, MongooseModule.forRoot(dbUrl)],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes(CaseController, ConditionController);
+  }
+}
