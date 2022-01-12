a05cd7e527010c33092fb21b8cd4e4f46adacc8b
mairaalves <marry.alves@hotmail.com>
2022-01-12 15:48:45 -0300

    [add] base frontend project

---
+++ front/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './routes/Home/Home';
+import Login from './routes/Login/Login'
+import './index.css';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+ReactDOM.render( <
+    BrowserRouter >
+    <
+    Routes >
+    <
+    Route exact path = "/"
+    element = { < Login / > }
+    /> <
+    Route exact path = "/home"
+    element = { < Home / > }
+    />  <
+    / Routes> <
+    / BrowserRouter>,
+    document.getElementById('root')
+);
